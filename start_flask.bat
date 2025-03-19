af36756ad5fde4a6f1c41f5107229c7ab185d583
DESKTOP-P71AP16\ASUS1 <tuconghieu1998@gmail.com>
2025-03-19 09:41:56 +0700

    views logs

--- start_flask.bat
+++ start_flask.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d D:\ESP32\Flask
-waitress-serve --port=5000 wsgi:app
+waitress-serve --port=5000 wsgi:app > waitress.log 2>&1
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
+import sys
 from main import app
+
+# Ensure print statements are immediately written to logs
+sys.stdout.reconfigure(line_buffering=True)
 
 if __name__ == "__main__":
     app.run()
